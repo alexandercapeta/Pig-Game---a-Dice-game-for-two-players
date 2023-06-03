30e6e8ed2a7401e375d52fc1cd1b3f5258dbaa53
Alexander Rivera <alexandercapeta@gmail.com>
2023-06-03 16:56:13 -0600

    Adding the random dice roll

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+// Rolling dice functionality.
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll.
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // 2. Display the dice.
+  //diceEl.classList.remove('hidden');
+  //diceEl.src = `dice-${dice}.png`;
+  // 3. Check for a roll 1: If true switch to next player.
+  /*if (dice !== 1) {
+    // Add the roll value to the current score.
+    currentScore += dice;
+  } else {
+  }*/
+});
